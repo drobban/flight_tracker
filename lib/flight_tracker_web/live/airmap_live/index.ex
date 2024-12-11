631c4456782845170241bc87e63139a5cc36e477
David Robertsson <david.robertsson@gmail.com>
2024-12-11 19:24:55 +0100

    Adding markers to the map.
    Simple test

--- lib/flight_tracker_web/live/airmap_live/index.ex
+++ lib/flight_tracker_web/live/airmap_live/index.ex
@@ -2,6 +2,7 @@ defmodule FlightTrackerWeb.AirmapLive.Index do
   use FlightTrackerWeb, :live_view
   require Logger
   alias FlightTrackerWeb.Components.LeafletMap, as: LeafletMap
+  alias Phoenix
 
   @impl true
   def mount(_params, _session, socket) do
@@ -11,13 +12,27 @@ defmodule FlightTrackerWeb.AirmapLive.Index do
     {:ok, socket}
   end
 
+  @doc """
+  This handle_event is just a simple test.
+  """
+  @impl true
+  def handle_event("add-plane", _unsigned_params, socket) do
+    socket =
+      socket
+      |> Phoenix.LiveView.push_event("add_marker", %{reference: "MH417", lat: 51.123, lon: 7.3})
+
+    {:noreply, socket}
+  end
+
   @impl true
   def render(assigns) do
-    Logger.debug(inspect assigns)
+    Logger.debug(inspect(assigns))
+
     ~H"""
-      <div class="h-full w-full justify-center items-center">
-        <LeafletMap.map class="h-full bg-gray" />
-      </div>
+    <div class="h-full w-full justify-center items-center">
+      <LeafletMap.map class="h-full bg-gray" />
+    </div>
+    <.button phx-click="add-plane">En till</.button>
     """
   end
 end
